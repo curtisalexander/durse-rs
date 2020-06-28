9e1b56afb356506942c88813afc7fb682473017e
Curtis Alexander <calex@calex.org>
2020-06-28 18:24:55 -0500

    First experiments with std::fs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+
+fn main() -> std::io::Result<()> {
+    let md = fs::metadata("derse.txt")?;
+
+    println!("file type: {:?}", md.file_type());
+    println!("is directory?: {:?}", md.is_dir());
+    println!("is file?: {:?}", md.is_file());
+    println!("len: {:?}", md.len());
+    println!("persmissions: {:?}", md.permissions());
+    // Unix => mtime field of stat
+    // Windows => ftLastWriteTime field
+    if let Ok(time) = md.modified() {
+        println!("modified time{:?}", time);
+    } else {
+        println!("Not supported on this platform");
+    }
+    // Unix => atime field of stat
+    // Windows => ftAccessTime field
+    if let Ok(time) = md.accessed() {
+        println!("accessed time{:?}", time);
+    } else {
+        println!("Not supported on this platform");
+    }
+    // Linux => btime field of statx
+    // Linux => birthtime field of stat
+    // Windows => ftCreationTime
+    if let Ok(time) = md.created() {
+        println!("created time{:?}", time);
+    } else {
+        println!("Not supported on this platform");
+    }
+    Ok(())
 }
