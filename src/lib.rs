48326fa1c552cafa14e79e37e67c63aab9dedfa9
Curtis Alexander <calex@calex.org>
2020-07-05 15:51:50 -0500

    Move cli logic and running into lib.rs; main only parses args, runs, and then handles any returned errors.

---
+++ src/lib.rs
@@ -0,0 +1,59 @@
+use std::path::Path;
+
+use clap::Clap;
+
+/// Get directory metadata
+#[derive(Clap)]
+// #[clap(version = "0.1.0", author = "Curtis Alexander <calex@calex.org>")]
+pub struct Args {
+    /// File for which to acquire metadata
+    #[clap(long, short)]
+    pub file: String,
+}
+
+pub fn run(args: Args) -> std::io::Result<()> {
+    println!("Value for file: {}", args.file);
+
+    let path = Path::new(&args.file);
+    let md = path.metadata()?;
+    // fs::metadata("derse.txt")?;
+
+    println!("file type: {:?}", md.file_type());
+    println!("is directory?: {:?}", md.is_dir());
+    println!("is file?: {:?}", md.is_file());
+    println!("len: {:?}", md.len());
+    println!("persmissions: {:?}", md.permissions());
+    // Unix => mtime field of stat
+    // Windows => ftLastWriteTime field
+    if let Ok(time) = md.modified() {
+        println!("modified time{:?}", time);
+    } else {
+        println!("Not supported on this platform");
+    }
+    // Unix => atime field of stat
+    // Windows => ftAccessTime field
+    if let Ok(time) = md.accessed() {
+        println!("accessed time{:?}", time);
+    } else {
+        println!("Not supported on this platform");
+    }
+    // Linux => btime field of statx
+    // Unix => birthtime field of stat
+    // Windows => ftCreationTime
+    if let Ok(time) = md.created() {
+        println!("created time{:?}", time);
+    } else {
+        println!("Not supported on this platform");
+    }
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    // use super::*;
+
+    #[test]
+    fn clap_parsing() {
+        assert_eq!(1, 1);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,13 @@
 use clap::Clap;
-// use std::fs;
-use std::path::Path;
 
-/// Get directory metadata
-#[derive(Clap)]
-// #[clap(version = "0.1.0", author = "Curtis Alexander <calex@calex.org>")]
-struct Args {
-    /// File for which to acquire metadata
-    #[clap(long, short)]
-    file: String,
-}
+use derse::Args;
+use std::process;
 
-fn main() -> std::io::Result<()> {
-    let opts: Args = Args::parse();
-
-    println!("Value for file: {}", opts.file);
-    let path = Path::new(&opts.file);
-    let md = path.metadata()?;
-    // fs::metadata("derse.txt")?;
-
-    println!("file type: {:?}", md.file_type());
-    println!("is directory?: {:?}", md.is_dir());
-    println!("is file?: {:?}", md.is_file());
-    println!("len: {:?}", md.len());
-    println!("persmissions: {:?}", md.permissions());
-    // Unix => mtime field of stat
-    // Windows => ftLastWriteTime field
-    if let Ok(time) = md.modified() {
-        println!("modified time{:?}", time);
-    } else {
-        println!("Not supported on this platform");
+fn main() {
+    let args: Args = Args::parse();
+    if let Err(e) = derse::run(args) {
+        println!("Stopping with error: {}", e);
+        process::exit(1);
     }
-    // Unix => atime field of stat
-    // Windows => ftAccessTime field
-    if let Ok(time) = md.accessed() {
-        println!("accessed time{:?}", time);
-    } else {
-        println!("Not supported on this platform");
-    }
-    // Linux => btime field of statx
-    // Unix => birthtime field of stat
-    // Windows => ftCreationTime
-    if let Ok(time) = md.created() {
-        println!("created time{:?}", time);
-    } else {
-        println!("Not supported on this platform");
-    }
-    Ok(())
+    process::exit(0);
 }
